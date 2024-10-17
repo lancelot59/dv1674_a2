563f033eabca31b96dbe6e49059800133eb45956
lancelot59 <teddan@uniqaq.se>
2024-10-17 15:04:44 +0200

    simd for mean

--- pearson/vector.cpp
+++ pearson/vector.cpp
@@ -65,16 +65,18 @@ double &Vector::operator[](unsigned i)
 
 double Vector::mean() const
 {
-    double sum{0};
+    __m256d sumVec = _mm256_setzero_pd();
+    int i = 0;
 
-    for (auto i{0}; i < size; i+=4)
+    for (; i <= size -4; i+=4)
     {
-        sum += data[i];
-        sum += data[i+1];
-        sum += data[i+2];
-        sum += data[i+3];
+        __m256d dataVec = _mm256_loadu_pd(&data[i]);
+        sumVec = _mm256_add_pd(sumVec, dataVec);
     }
-
+    double sum = 0;
+    double temp[4];
+    _mm256_storeu_pd(temp, sumVec);
+    sum += temp[0] + temp[1] + temp[2] + temp[3];
     return sum / static_cast<double>(size);
 }
 
