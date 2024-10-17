4f1fd62d26fb513603b9fe5adf8d1d5e7560dcf3
mirawoznica <m.woznica@student.fontys.nl>
2024-10-17 21:25:37 +0200

    Added threads to do some rows at the same time

--- blur/filters.cpp
+++ blur/filters.cpp
@@ -6,6 +6,9 @@ Author: David Holmqvist <daae19@student.bth.se>
 #include "matrix.hpp"
 #include "ppm.hpp"
 #include <cmath>
+#include <pthread.h>
+#include <vector>
+
 
 namespace Filter
 {
@@ -22,27 +25,42 @@ namespace Filter
         }
     }
 
-    Matrix blur(Matrix m, const int radius)
-    {
-        Matrix scratch{PPM::max_dimension};
-        auto dst{m};
+    struct ThreadData {
+        Matrix* dst;
+        Matrix* scratch;
+        double* w;
+        int radius;
+        unsigned x_size;
+        unsigned y_size;
+        int start;
+        int end;
+        ThreadData()
+            : dst(nullptr), scratch(nullptr), w(nullptr), radius(0),
+            x_size(0), y_size(0), start(0), end(0) {}
+        ThreadData(Matrix* d, Matrix* s, double* weights, int r, unsigned xs, unsigned ys, int st, int en)
+            : dst(d), scratch(s), w(weights), radius(r), x_size(xs), y_size(ys), start(st), end(en) {}
+    };
 
-        // 1 adding getting date before the loop
-        auto x_size {dst.get_x_size()};
-        auto y_size {dst.get_y_size()};
+    void* thread1(void* arg) {
+        ThreadData* data = static_cast<ThreadData*>(arg);
+        Matrix* dst = data->dst;
+        Matrix* scratch = data->scratch;
+        double* w = data->w;
+        const int radius = data->radius;
+        unsigned x_size = data->x_size;
+        unsigned y_size = data->y_size;
+        int start = data->start;
+        int end = data->end;
 
-        // 2 Gauss operation before the loop not inside
-        double w[Gauss::max_radius]{};
-        Gauss::get_weights(radius, w);
         // 3 move this outside of the loop
         auto w0 {w[0]};
 
         //4 Moved those calling for those arrays outside of the loop. They are called once instead of multiple times
-        auto R_dst = dst.get_R(), G_dst = dst.get_G(), B_dst = dst.get_B();
-        auto R_scratch = scratch.get_R(), G_scratch = scratch.get_G(), B_scratch = scratch.get_B();
+        auto R_dst = dst->get_R(), G_dst = dst->get_G(), B_dst = dst->get_B();
+        auto R_scratch = scratch->get_R(), G_scratch = scratch->get_G(), B_scratch = scratch->get_B();
 
-        // dst loop
-        for (auto x{0}; x < x_size; x++)
+
+         for (auto x{start}; x < end; x++)
         {
             for (auto y{0}; y < y_size; y++)
             {
@@ -79,7 +97,7 @@ namespace Filter
             }
         }
 
-        for (auto x{0}; x < x_size; x++)
+        for (auto x{start}; x < end; x++)
         {
             for (auto y{0}; y < y_size; y++)
             {
@@ -113,6 +131,41 @@ namespace Filter
                 G_dst[index] = g / n;
                 B_dst[index] = b / n;
             }
+        }
+        return nullptr;
+
+    }
+
+    Matrix blur(Matrix m, const int radius)
+    {
+        Matrix scratch{PPM::max_dimension};
+        auto dst{m};
+
+        // 1 adding getting date before the loop
+        auto x_size {dst.get_x_size()};
+        auto y_size {dst.get_y_size()};
+
+        // 2 Gauss operation before the loop not inside
+        double w[Gauss::max_radius]{};
+        Gauss::get_weights(radius, w);
+
+        //5 removed the const from getter for R, G, and B in matrix.cpp
+        const int num_threads = 4;
+        pthread_t threads[num_threads];
+        std::vector<ThreadData> thread_data(num_threads);
+
+        int rows = x_size / num_threads;
+
+        for(int i = 0; i < num_threads; i++) {
+            thread_data[i] = {&scratch, &dst, w, radius, x_size, y_size, i * rows, (i + 1) * rows};
+            if (i == num_threads - 1) {
+                thread_data[i].end = y_size;
+            }
+            pthread_create(&threads[i], nullptr, thread1, &thread_data[i]);
+        }
+
+        for (int i = 0; i < num_threads; i++) {
+            pthread_join(threads[i], nullptr);
         }
 
         return dst;
